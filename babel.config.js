07ca23580cb7b8b2d04fd13205bdb7e29308071b
https-dre <diaso.andre@outlook.com>
2025-09-28 20:35:41 -0300

    add dotenv on babel config

---
+++ babel.config.js
@@ -0,0 +1,17 @@
+module.exports = function (api) {
+  api.cache(true);
+  return {
+    presets: ["babel-preset-expo"],   // obrigatório no Expo
+    plugins: [
+      [
+        "module:react-native-dotenv",
+        {
+          moduleName: "@env",
+          path: ".env",
+          safe: false,
+          allowUndefined: true,
+        },
+      ],
+    ],
+  };
+};
