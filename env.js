862fb3e4abd9bf22f2351388c4a7a41f2cac4687
leonardo8896 <pkafonso8896@gmail.com>
2025-09-29 20:55:05 -0300

    test: add basic environment setup

--- env.js
+++ env.js
@@ -1,16 +1,21 @@
-import { z } from "zod";
+/* import { z } from "zod";
 
 import { API_ADDRESS, WS_SERVER } from "@env";
-
+ */
 let ENV = null;
 
-const envSchema = z.object({
+/* const envSchema = z.object({
   WS_SERVER: z.url(),
   API_ADDRESS: z.url()
 });
 
 ENV = envSchema.parse( { API_ADDRESS, WS_SERVER });
 
-ENV.WS_SERVER = "wss://exemptible-odilia-sorely.ngrok-free.dev/"
+ENV.WS_SERVER = "wss://exemptible-odilia-sorely.ngrok-free.dev/" */
+
+ENV = {
+  WS_SERVER: "https://exemptible-odilia-sorely.ngrok-free.dev/",
+  API_ADDRESS: "https://megascopic-anahi-foamlike.ngrok-free.dev/"
+}
 
 export { ENV }
