07ebbce5e944745c35fde405517fe4fdc870bac9
leonardo8896 <pkafonso8896@gmail.com>
2025-10-13 08:15:02 -0300

    update environment configuration for WS_SERVER and API_ADDRESS

--- env.js
+++ env.js
@@ -14,8 +14,8 @@ ENV = envSchema.parse( { API_ADDRESS, WS_SERVER });
 ENV.WS_SERVER = "wss://exemptible-odilia-sorely.ngrok-free.dev/" */
 
 ENV = {
-  WS_SERVER: "ws://52.67.221.152:9003/ws",
-  API_ADDRESS: "https://exemptible-odilia-sorely.ngrok-free.dev/api"
+  WS_SERVER: "ws://hydrignis.ddns.net/ws",
+  API_ADDRESS: "https://exemptible-odilia-sorely.ngrok-free.dev"
 }
 
 export { ENV }
